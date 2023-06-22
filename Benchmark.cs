4307bdf33807bb085de11aae3bac959de4301955
Vangelis49 <114907318+Vangelis49@users.noreply.github.com>
2023-06-22 09:36:47 +0100

    Add files via upload

---
+++ Benchmark.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using UndirectedWeightedGraph;
+
+namespace Testing
+{
+    internal class Benchmark
+    {
+        List<long> execTime_v_1 = new List<long>();
+        List<long> execTime_v_2 = new List<long>();
+        List<long> execTime_v_3 = new List<long>();
+
+        int runTest = 10;
+
+        public void Benchmark_V_1(TFLShortestPathFinder_matrix.Station source,
+            TFLShortestPathFinder_matrix.Station target,
+            TFLShortestPathFinder_matrix.Graph graph, int[,] adj)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            sw.Start();
+
+            TFLShortestPathFinder_matrix.MyList<string>[] resp_v_1 = graph.DijkestraAlgorithm(source, target, graph, adj);
+
+            sw.Stop();
+            execTime_v_1.Add(sw.ElapsedMilliseconds);
+        }
+
+        public void Benchmark_V_2(UndirectedWeightedGraph.Station source,
+            UndirectedWeightedGraph.Station target,
+            UndirectedWeightedGraph.Graph graph, UndirectedWeightedGraph.MyList<MyLinkedList> adj)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            sw.Start();
+
+            UndirectedWeightedGraph.MyList<string>[] resp_v_2 = graph.DijkstraAlgorithem(source, target, graph, adj);
+
+            sw.Stop();
+            execTime_v_2.Add(sw.ElapsedMilliseconds);
+        }
+
+        public void Benchmark_V_3(TFLShortestPathFinder.Station source,
+            TFLShortestPathFinder.Station target,
+            TFLShortestPathFinder.Graph graph, int[,] adj)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            sw.Start();
+
+            List<string>[] resp_v_3 = graph.DijkestraAlgorithm(source, target, graph, adj);
+
+            sw.Stop();
+            execTime_v_3.Add(sw.ElapsedMilliseconds);
+        }
+
+        public void RunTest()
+        {
+            TFLShortestPathFinder_matrix.Graph graph_matrix = new TFLShortestPathFinder_matrix.Graph();
+            graph_matrix = graph_matrix.CreateGraph(graph_matrix);
+            int[,] adj_m = graph_matrix.CreateAdjMatrix();
+
+            UndirectedWeightedGraph.Graph graph_list = new UndirectedWeightedGraph.Graph();
+            graph_list = graph_list.CreateGraph(graph_list);
+            UndirectedWeightedGraph.MyList<UndirectedWeightedGraph.MyLinkedList> adj_l = graph_list.CreateAdjList();
+
+            TFLShortestPathFinder.Graph graph_net = new TFLShortestPathFinder.Graph();
+            graph_net = graph_net.CreateGraph(graph_net);
+            int[,] adj_n = graph_net.CreateAdjMatrix();
+
+            Console.WriteLine("Enter the source station name:");
+            string sourceName = Console.ReadLine();
+            Console.WriteLine("Enter the target station name:");
+            string targetName = Console.ReadLine();
+
+            TFLShortestPathFinder_matrix.Station source_m = TFLShortestPathFinder_matrix.Station.FindStationByName(sourceName, graph_matrix.stations);
+            TFLShortestPathFinder_matrix.Station target_m = TFLShortestPathFinder_matrix.Station.FindStationByName(targetName, graph_matrix.stations);
+
+            UndirectedWeightedGraph.Station source_l = graph_list.GetStationByName(sourceName);
+            UndirectedWeightedGraph.Station target_l = graph_list.GetStationByName(targetName);
+
+            TFLShortestPathFinder.Station source_n = TFLShortestPathFinder.Station.FindStationByName(sourceName, graph_net.stations);
+            TFLShortestPathFinder.Station target_n = TFLShortestPathFinder.Station.FindStationByName(targetName, graph_net.stations);
+
+            for (int i = 0; i < runTest; i++)
+            {
+                Benchmark_V_1(source_m, target_m, graph_matrix, adj_m);
+                Benchmark_V_2(source_l, target_l, graph_list, adj_l);
+                Benchmark_V_3(source_n, target_n, graph_net, adj_n);
+            }
+        }
+
+        public void OutputResults()
+        {
+            Console.WriteLine("\nResults:");
+
+            Console.WriteLine($"Version 1: {execTime_v_1.Average()} ms");
+            Console.WriteLine($"Version 2: {execTime_v_2.Average()} ms");
+            Console.WriteLine($"Version 3: {execTime_v_3.Average()} ms");
+        }
+    }
+}
---
+++ TestVersion_1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TFLShortestPathFinder_matrix; //version 1
+using UndirectedWeightedGraph; // Version_2
+
+
+namespace Testing
+{
+    internal class TestVersion_1
+    {
+        TFLShortestPathFinder_matrix.MyList<string>[] resp_v_1;
+        UndirectedWeightedGraph.MyList<string>[] resp_v_2;
+        List<string>[] resp_v_3;
+
+        // For Matrix
+        public void Test_V_1()
+        {
+            TFLShortestPathFinder_matrix.Graph graph = new TFLShortestPathFinder_matrix.Graph();
+
+            graph = graph.CreateGraph(graph);
+            int[,] adj = graph.CreateAdjMatrix();
+
+            // Call the function to find the fastest walking route
+            Console.WriteLine("Enter the source station name:");
+            string sourceName = Console.ReadLine();
+            Console.WriteLine("Enter the target station name:");
+            string targetName = Console.ReadLine();
+
+
+            TFLShortestPathFinder_matrix.Station source = TFLShortestPathFinder_matrix.Station.FindStationByName(sourceName, graph.stations);
+            TFLShortestPathFinder_matrix.Station target = TFLShortestPathFinder_matrix.Station.FindStationByName(targetName, graph.stations);
+
+            if (source == null || target == null)
+            {
+                Console.WriteLine("Invalid source or target station name. Please make sure you entered the correct names.");
+                return; // or you can use a loop to ask for input again
+            }
+
+            resp_v_1 = graph.DijkestraAlgorithm(source, target, graph, adj);
+        }
+
+        // Adj List Version
+        public void Test_V_2()
+        {
+
+            UndirectedWeightedGraph.Graph graph = new UndirectedWeightedGraph.Graph();
+            graph = graph.CreateGraph(graph);
+
+            UndirectedWeightedGraph.MyList<UndirectedWeightedGraph.MyLinkedList> adj = graph.CreateAdjList();
+
+            // Call the function to find the fastest walking route
+            Console.WriteLine("Enter the source station name:");
+            string sourceName = Console.ReadLine();
+            Console.WriteLine("Enter the target station name:");
+            string targetName = Console.ReadLine();
+
+            UndirectedWeightedGraph.Station source = graph.GetStationByName(sourceName);
+            UndirectedWeightedGraph.Station target = graph.GetStationByName(targetName);
+
+            resp_v_2 = graph.DijkstraAlgorithem(source, target, graph, adj);
+        }
+
+        public void Test_V_3()
+        {
+            TFLShortestPathFinder.Graph graph = new TFLShortestPathFinder.Graph();
+
+            graph = graph.CreateGraph(graph);
+            int[,] adj = graph.CreateAdjMatrix();
+
+            // Call the function to find the fastest walking route
+            Console.WriteLine("Enter the source station name:");
+            string sourceName = Console.ReadLine();
+            Console.WriteLine("Enter the target station name:");
+            string targetName = Console.ReadLine();
+
+
+            TFLShortestPathFinder.Station source = TFLShortestPathFinder.Station.FindStationByName(sourceName, graph.stations);
+            TFLShortestPathFinder.Station target = TFLShortestPathFinder.Station.FindStationByName(targetName, graph.stations);
+
+            if (source == null || target == null)
+            {
+                Console.WriteLine("Invalid source or target station name. Please make sure you entered the correct names.");
+                return; // or you can use a loop to ask for input again
+            }
+
+
+            resp_v_3 = graph.DijkestraAlgorithm(source, target, graph, adj);
+        }
+
+        public void ComparePaths()
+        {
+            for (int i = 0; i < resp_v_3[0].Count(); i++)
+            {
+                if (resp_v_1[0][i] == resp_v_2[0][i] && resp_v_1[0][i] == resp_v_3[0][i] && resp_v_2[0][i] == resp_v_3[0][i])
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Path did not match!");
+                }
+            }
+
+            Console.WriteLine("All the Three Versions Produce the Same Paths");
+        }
+
+        public void CompareTime()
+        {
+            if (resp_v_1[1][1] != resp_v_2[1][1] && resp_v_1[1][1] != resp_v_3[1][1] && resp_v_2[1][1] != resp_v_3[1][1])
+            {
+                Console.WriteLine("Time did not match!");
+            }
+            else
+            {
+                Console.WriteLine("All the Three Versions Produce the Same Section Timings and Overall Fastest Time");
+            }
+        }
+
+    }
+}
